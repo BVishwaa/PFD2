86b7f92fdaf95bb8cf55bb7ef34eb768bfe57654
yukiii18 <110827731+yukiii18@users.noreply.github.com>
2024-01-22 13:07:13 +0800

    creating view for creation of post (forum)

---
+++ EldenGuide/Models/Threads.cs
@@ -0,0 +1,17 @@
+﻿namespace EldenGuide.Models
+{
+    public class Threads
+    {
+        public string ThreadId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Username { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DatePosted { get; set; }
+    }
+}
