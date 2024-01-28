f25c2732474a4fb197733baf2fc3e0f99a814c74
BVishwaa <S10240086@connect.np.edu.sg>
2024-01-28 17:42:06 +0800

    TOC loading( Progress in GuideEditing also)

--- EldenGuide/Models/Threads.cs
+++ EldenGuide/Models/Threads.cs
@@ -14,5 +14,9 @@ namespace EldenGuide.Models
         public string Category { get; set; }
         [FirestoreProperty]
         public string Description { get; set; }
+        [FirestoreProperty]
+        public string DatePosted { get; set; }
+        [FirestoreProperty]
+        public string Username { get; set; }
     }
 }
