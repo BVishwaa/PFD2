1f396a897370551cfc78a7e06aa92c279848fb9d
Jermy-Lee <116236000+Jermy-Lee@users.noreply.github.com>
2024-01-31 23:16:29 +0800

    push chathub

--- EldenGuide/Hubs/ChatHub.cs
+++ EldenGuide/Hubs/ChatHub.cs
@@ -6,6 +6,7 @@ namespace EldenGuide.Hubs
     {
         public async Task SendMessage(string user, string message)
         { 
+            Console.WriteLine("jeremy is fat");
             Console.WriteLine("jeremy is wrong" + message);
             Clients.All.SendAsync("ReceiveMessage", user, message);
 
