981163ff8e99e3ccae687a9d0e322e5913a3ef26
BVishwaa <S10240086@connect.np.edu.sg>
2024-02-03 13:43:25 +0800

    Auth fixed, search feature fixed, Guides also dynamically load in index page and session management fixed

--- EldenGuide/Controllers/HomeController.cs
+++ EldenGuide/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using EldenGuide.Models;
+﻿using EldenGuide.DAL;
+using EldenGuide.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,9 +14,12 @@ namespace EldenGuide.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Guide> guideList = new List<Guide>();  
+            CategoryDAL categoryDAL = new CategoryDAL();
+            guideList = await categoryDAL.OrganizeAllGuides();
+            return View(guideList);
         }
 
         public IActionResult About()
@@ -37,6 +41,62 @@ namespace EldenGuide.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> StaffLogin()
+        {
+            Staff staff = new Staff();
+            return View(staff);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> StaffLogin(IFormCollection form)
+        {
+            AuthDAL staffDAL = new AuthDAL();
+            Staff staff = await staffDAL.GetStaffByEmail(form["Email"]);
+
+            if (staff != null && staff.Password == form["Password"])
+            {
+                // Logic for successful login
+                // Convert staff details to JSON (excluding password for security)
+                /*var staffData = new
+                {
+                    staff.Username,
+                    staff.Email
+                    // Add other required details but exclude sensitive information like password
+                };
+                string staffJson = System.Text.Json.JsonSerializer.Serialize(staffData);
+
+                // Set the staff session
+                HttpContext.Session.SetString("StaffSession", staffJson);*/
+
+                HttpContext.Session.SetString("staffUsername", staff.Username);
+                HttpContext.Session.SetString("staffEmail", staff.Email);
+
+
+                // Redirect to a secure staff page after login
+                return RedirectToAction("StaffGuideList", "Guide");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(); // Return to staff login view
+            }
+
+            //Staff staff = new Staff();
+            //AuthDAL staffDAL = new AuthDAL();
+
+            ///*user.Username = form["Username"];*/      //Call out the form in the Auth View page to instantiate the properties in the model object created
+            //staff.Email = form["Email"];
+            //staff.Password = form["Password"];
+
+            //await staffDAL.AddStaff(staff);
+
+
+            //Debug.WriteLine("Debug statement: Something happened!");
+            //Debug.WriteLine("Nice statement:Staff Login!");
+            //return View();
         }
     }
 }
